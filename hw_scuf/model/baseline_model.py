d6bbc396658fd7e8b33ae82dd1681add2bb0da3b
mcnckc <s20b1_gusev@179.ru>
2023-12-16 01:14:42 +0300

    debug

--- hw_scuf/model/baseline_model.py
+++ hw_scuf/model/baseline_model.py
@@ -18,6 +18,7 @@ class BaselineModel(BaseModel):
         )
 
     def forward(self, spectrogram, **batch):
+        print("RECEIVED SHAPE", spectrogram.shape)
         return {"logits": self.net(spectrogram.transpose(1, 2))}
 
     def transform_input_lengths(self, input_lengths):
