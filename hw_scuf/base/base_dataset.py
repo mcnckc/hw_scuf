751c811dfe6d9c5535fc2d1292990fdd3b56f27e
mcnckc <s20b1_gusev@179.ru>
2023-12-16 16:29:31 +0300

    debug

--- hw_scuf/base/base_dataset.py
+++ hw_scuf/base/base_dataset.py
@@ -40,7 +40,6 @@ class BaseDataset(Dataset):
         self._index: List[dict] = index
         print("SCUF AMOUNT:", np.sum([item['target'] for item in index]))
         print("SCUF PART:", np.mean([item['target'] for item in index]))
-        print("INDEX:", [item['target'] for item in index])
 
     def __getitem__(self, ind):
         data_dict = self._index[ind]
