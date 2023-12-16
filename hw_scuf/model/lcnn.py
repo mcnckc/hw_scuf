2e20c747b30753ca399cd750632d8e879d7c1017
mcnckc <s20b1_gusev@179.ru>
2023-12-16 18:02:34 +0300

    update model

--- hw_scuf/model/lcnn.py
+++ hw_scuf/model/lcnn.py
@@ -33,45 +33,58 @@ class BatchNorm0d(nn.Module):
 class LCNN(BaseModel):
     def __init__(self, H, W, **batch):
         super().__init__(**batch)
+        nH, nW = H, W
+        for _ in range(4):
+            nH //= 2
+            nW //= 2
         self.net = Sequential(
-            TransposeChannels(),
             nn.Conv2d(1, 64, 5, 1, padding='same'), #1
             MFM(),                                  #2
             nn.MaxPool2d(2, padding='same'),        #3
+
             nn.Conv2d(32, 64, 1, 1, padding='same'),#4
             MFM(),                                  #5
             nn.BatchNorm2d(32),                     #6
             nn.Conv2d(32, 96, 3, 1, padding='same'),#7
             MFM(),                                  #8
+
             nn.MaxPool2d(2, padding='same'),        #9
             nn.BatchNorm2d(48),                     #10
+
             nn.Conv2d(48, 96, 1, 1, padding='same'),#11
             MFM(),                                  #12
             nn.BatchNorm2d(48),                     #13
             nn.Conv2d(48, 128, 3, 1, padding='same'),#14
             MFM(),                                  #15
+
             nn.MaxPool2d(2, padding='same'),        #16
-            nn.Conv2d(64, 64, 3, 1, padding='same'),#17
+
+            nn.Conv2d(64, 64, 1, 1, padding='same'),#17
             MFM(),                                  #18
             nn.BatchNorm2d(32),                     #19
-            nn.Conv2d(32, 64, 1, 1, padding='same'),#20
+            nn.Conv2d(32, 64, 3, 1, padding='same'),#20
             MFM(),                                  #21
+
             nn.BatchNorm2d(32),                     #22
-            nn.Conv2d(32, 64, 3, 1, padding='same'),#23
+
+            nn.Conv2d(32, 64, 1, 1, padding='same'),#23
             MFM(),                                  #24
             nn.BatchNorm2d(32),                     #25
             nn.Conv2d(32, 64, 3, 1, padding='same'),#26
             MFM(),                                  #27
+            nn.Dropout(0.1),
+            nn.Conv2d(32, 32, 1, 1, padding='same'),
+
             nn.MaxPool2d(2, padding='same'),        #28
             nn.Flatten(),
-            nn.Linear(H * W * 32, 160),             #29
+            nn.Linear(nH * nW * 32, 160),             #29
             MFM(),                                  #30
             BatchNorm0d(80),                        #31
             nn.Linear(80, 2)                        #32
         )
 
     def forward(self, spectrogram, **batch):
-        out = self.net(spectrogram.transpose(1, 2))
+        out = self.net(spectrogram[:, None, :, :])
         return {"logits": out / torch.linalg.vector_norm(self.net[-1].weights, dim=0)}
 
     def transform_input_lengths(self, input_lengths):
