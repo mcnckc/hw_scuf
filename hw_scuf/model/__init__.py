ff4219d04b6e2565a8b66fcb698f0f2299cd8b81
mcnckc <s20b1_gusev@179.ru>
2023-12-16 18:59:06 +0300

    no dropout

--- hw_scuf/model/__init__.py
+++ hw_scuf/model/__init__.py
@@ -1,7 +1,9 @@
 from hw_scuf.model.baseline_model import BaselineModel
 from hw_scuf.model.lcnn import LCNN
+from hw_scuf.model.lcnn_no_dropout import LCNNNoDropout
 
 __all__ = [
     "BaselineModel",
-    "LCNN"
+    "LCNN",
+    "LCNNNoDropout"
 ]
